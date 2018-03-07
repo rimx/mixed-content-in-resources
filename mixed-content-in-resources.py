2ff70f644fd762bc573fba84e50cf38f71f0f9b9
Rafi Islam <rafi.islam@statravel.com>
2018-03-07 17:51:47 +0100

    intial commit

---
+++ mixed-content-in-resources.py
@@ -0,0 +1,37 @@
+import sys
+import re
+import urllib.request
+
+exp = 'http?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+counter = 1
+
+if len(sys.argv) < 2: 
+  print("Please pass a .txt file, containing URL's (1 per line) as the first argument")
+  quit()
+else:
+  input_file = sys.argv[1]
+  print('\n'*3)
+  print('*'*72 + "\n\nStart crawling URL's in " + input_file + '\n\n' + '*'*72 + '\n')
+with open('output_'+input_file, 'w') as output_file:
+  with open(input_file) as f:
+    for line in f:
+      line = line.strip()
+      print('\n'+line)
+      print('-'*72)
+      try:
+        content = urllib.request.urlopen(line).read().decode('utf-8')
+        urls = re.findall(exp,content)
+        if urls:        
+          output_file.write(str(counter) + '.' +line+'\n' + '-'*72+'\n')
+          for pos,url in enumerate(urls):
+            print(url)
+            output_file.write(url+'\n')
+            if pos == len(urls) -1:
+              output_file.write('\n'*2)
+          counter +=1
+        else:
+          print ('This file is clean')
+      except urllib.error.URLError as e:
+        print(e.reason)
+        output_file.write(line +'\n' + '-'*72+'\n' + e.reason + '\n')
+
